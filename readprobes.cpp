fa31bf1be9ef6c732bb583d8b8675d4d8bd48c93
dimarafael <dimarafael@gmail.com>
2024-04-02 13:43:25 +0200

    implemented data reading from modbus

--- readprobes.cpp
+++ readprobes.cpp
@@ -3,7 +3,10 @@
 
 ReadProbes::ReadProbes()
 {
-
+    mc = new QModbusTcpClient(this);
+    mc->setConnectionParameter(QModbusDevice::NetworkAddressParameter, "10.0.14.88");
+    mc->setConnectionParameter(QModbusDevice::NetworkPortParameter, 1502);
+    connect(mc, &QModbusClient::stateChanged, this, &ReadProbes::processStateChanged);
 }
 
 ReadProbes::~ReadProbes()
@@ -13,6 +16,10 @@ ReadProbes::~ReadProbes()
 
 void ReadProbes::run()
 {
+#ifndef SIMULATION
+    mc->connectDevice();
+#endif
+    m_currentDevice = 0;
     m_probes = new QVector<ProbeData>(N_PROBES);
     m_timer = new QTimer(this);
     m_timer->setInterval(5000);
@@ -37,5 +44,139 @@ void ReadProbes::readData()
     emit dataReady(*m_probes);
 #endif
 
+#ifndef SIMULATION
+    if(mc->state() == QModbusDevice::ConnectedState){
 
+        // for(int i = 0; i < N_PROBES; i++){
+
+
+        //     // vData = new QVector<quint16>(7);
+
+        //     du = new QModbusDataUnit(QModbusDataUnit::HoldingRegisters, (12188 + 150 * (i + 1)) ,QVector<quint16>(1));
+
+
+        //     // if(du == nullptr) delete du;
+        //     // du = new QModbusDataUnit(QModbusDataUnit::HoldingRegisters, (12201 + 150 * (i + 1)) ,*vData);
+        // }
+
+
+        // du = new QModbusDataUnit(QModbusDataUnit::HoldingRegisters, (12188 + 150 * (0 + 1)) ,QVector<quint16>(1));
+        // auto *reply = mc->sendReadRequest(*du,1);
+        // if(reply->isFinished()){
+        //     qDebug() << "reply->isFinished()";
+        //     delete reply;
+        // } else{
+        //     qDebug() << "NOT reply->isFinished()";
+        //     // connect(reply, &QModbusReply::finished, this, &modbus::processReplyResult);
+        // }
+
+        if(du == nullptr) delete du;
+        du = new QModbusDataUnit(QModbusDataUnit::HoldingRegisters, (12188 + 150 * (m_currentDevice + 1)) ,QVector<quint16>(1));
+        if(auto *reply = mc->sendReadRequest(*du,1)){
+            if(reply->isFinished()){
+                delete reply;
+            } else{
+                connect(reply, &QModbusReply::finished, this, &ReadProbes::processOnlineStatus);
+            }
+        } else {
+            qDebug() << "Reply error for sensor" << m_currentDevice << " : " << mc->errorString();
+            incrementCurrentDevice(false);
+        }
+
+
+    } else if (mc->state() == QModbusDevice::UnconnectedState){
+        mc->connectDevice();
+    }
+
+#endif
+
+}
+
+//ModbusClient state changed
+void ReadProbes::processStateChanged(QModbusDevice::State state)
+{
+    qDebug() << "Modbus connection: " << mc->state();
+    if(mc->state() == QModbusDevice::ConnectedState){
+        emit updateConnectedState(true);
+    } else{
+        emit updateConnectedState(false);
+    }
+}
+
+void ReadProbes::processOnlineStatus()
+{
+    auto reply = qobject_cast<QModbusReply *>(sender());
+    if (!reply || reply->error()){
+        incrementCurrentDevice(false);
+        return;
+    }
+
+    if(!reply->error()){
+        if((reply->result().values()[0] & (1 << 9)) == 512){ // 512 - probe online
+            qDebug() << "Sensor " << QString::number(m_currentDevice) << " online";
+            // probe connected, try to read data about temperatures
+            if(mc->state() == QModbusDevice::ConnectedState){
+
+                if(du == nullptr) delete du;
+                du = new QModbusDataUnit(QModbusDataUnit::HoldingRegisters, (12201 + 150 * (m_currentDevice + 1)) ,QVector<quint16>(7));
+                if(auto *reply = mc->sendReadRequest(*du,1)){
+                    if(reply->isFinished()){
+                        delete reply;
+                        incrementCurrentDevice(false);
+                    } else{
+                        connect(reply, &QModbusReply::finished, this, &ReadProbes::processTemperatures);
+                    }
+                } else {
+                    qDebug() << "Reply error for sensor" << m_currentDevice << " : " << mc->errorString();
+                    incrementCurrentDevice(false);
+                }
+            } else{
+                incrementCurrentDevice(false);
+                if (mc->state() == QModbusDevice::UnconnectedState) mc->connectDevice();
+            }
+
+        } else{
+            incrementCurrentDevice(false);
+        }
+    }
+    reply->deleteLater();
+}
+
+void ReadProbes::processTemperatures()
+{
+    auto reply = qobject_cast<QModbusReply *>(sender());
+    if (!reply || reply->error()){
+        incrementCurrentDevice(false);
+        return;
+    }
+    if(!reply->error()){
+        ProbeData tmp_probeData;
+        tmp_probeData.setOnline(true);
+        tmp_probeData.setT1((float)(reply->result().values()[2])/10);
+        tmp_probeData.setT2((float)(reply->result().values()[4])/10);
+        tmp_probeData.setT3((float)(reply->result().values()[1])/10);
+        tmp_probeData.setT4((float)(reply->result().values()[0])/10);
+        tmp_probeData.setBattery(reply->result().values()[6]);
+        m_probes->replace(m_currentDevice, tmp_probeData);
+        incrementCurrentDevice(true);
+    }
+    reply->deleteLater();
+}
+
+void ReadProbes::incrementCurrentDevice(bool isOnline)
+{
+    if(!isOnline){
+        ProbeData tmp_probeData;
+        tmp_probeData.setOnline(false);
+        m_probes->replace(m_currentDevice, tmp_probeData);
+    }
+
+    if(m_currentDevice < N_PROBES - 1){
+        m_currentDevice++;
+        readData();
+    }
+    else{
+        m_currentDevice = 0;
+        emit dataReady(*m_probes);
+    }
 }
--- readprobes.h
+++ readprobes.h
@@ -6,10 +6,12 @@
 #include <QDebug>
 #include <QRandomGenerator>
 #include "probedata.h"
+#include <QVariant>
+#include <QModbusTcpClient>
 
 #define N_PROBES 12
 
-#define SIMULATION
+// #define SIMULATION
 
 class ReadProbes : public QObject
 {
@@ -21,15 +23,25 @@ public:
 
 signals:
     void dataReady(QVector<ProbeData> data);
+    void updateConnectedState(bool connected);
 public slots:
     void run(); // for starting thread
 
 private slots:
     void readData(); // connect to timeout of timer
+    void processStateChanged(QModbusClient::State state);
+    void processOnlineStatus();
+    void processTemperatures();
 
 private:
     QVector<ProbeData> *m_probes;
     QTimer *m_timer;
+    QModbusTcpClient *mc = nullptr;
+    QVector<quint16> *vData;
+    QModbusDataUnit *du;
+    int m_currentDevice; // for polling devices in loop
+
+    void incrementCurrentDevice(bool isOnline); // if not online, set this device as offline
 };
 
 #endif // READPROBES_H
